5da65803b51ac95d62a0964aaa2f30b522dbb272
Dara Anggi Puspa <154232666+daraanggi@users.noreply.github.com>
2025-06-12 16:47:03 +0700

    Mengubah welcome menjadi auth.login

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,16 @@ use App\Http\Controllers\ProdukController;  // <-- Tambahkan ini
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/homepage', function () {
     return view('page.homepage');
 })->middleware(['auth', 'verified'])->name('page.homepage');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 
 Route::middleware('auth')->group(function () {
