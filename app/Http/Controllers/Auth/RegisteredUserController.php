0a2651fddee2fd5946d8b5cb56d4e6b22260cbab
Dara Anggi Puspa <154232666+daraanggi@users.noreply.github.com>
2025-06-12 16:45:09 +0700

    Menambahkan atribut no_telepon

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'no_telepon' => ['required', 'string'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'no_telepon' => $request->no_telepon,  
             'password' => Hash::make($request->password),
         ]);
 
