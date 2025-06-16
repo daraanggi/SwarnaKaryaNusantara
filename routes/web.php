ba988fa90985d2f51c46826d331cf69844c9674e
Dara Anggi Puspa <154232666+daraanggi@users.noreply.github.com>
2025-06-16 17:34:14 +0700

    Memperbaiki routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
 Route::get('/cek', function () {
     return view('/pembeliView/editProfile');
 });
